c7bde33936722ef2fd8a1429a8dbab3bff793492
Zachary Williams <kcaz311@gmail.com>
2021-08-23 15:12:00 -0400

    installing aws

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5'
+import Amplify from 'aws-amplify'
+import config from './src/aws-exports'
+Amplify.configure(config)
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Icon name="folder" size={30} color="#900" />
     </View>
   );
 }
